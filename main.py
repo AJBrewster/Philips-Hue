0b0c18769a2322e737398dd42f09124236b41d14
leemur <jon.lee.murray@gmail.com>
2017-09-03 12:04:19 -0300

    power on/off all working!

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
+import json
 import requests
 
 def powerOnAll(power):
     base_url = 'http://192.168.2.22/api/MhMITSZ9EgJ2z45790rlMTZi02sXlznW82AP6Yln/'
     lights_get = requests.get(base_url + 'lights')
     lights = lights_get.json()
-    payload = {'on': power}
+    payload = json.dumps({'on': power})
 
     # [{'error': {'type': 2, 'description': 'body contains invalid json', 'address': '/2/state'}}]
     # could be something wrong with :payload: or :url:
-        for light in lights:
-        url = base_url + light + '/state'
+    for light in lights:
+        url = base_url + 'lights/' + light + '/state'
         light_put = requests.put(url, data=payload)
         print(light_put.json())
 
@@ -26,6 +27,6 @@ def printLightInfo():
 
 if __name__ == '__main__':
     print('\n--- Power On ---')
-    powerOnAll(False)
+    powerOnAll(True)
     print('\n--- Light Information ---')
     printLightInfo()
