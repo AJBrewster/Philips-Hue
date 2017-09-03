fe375844ecf5a276add0414b1af6cf4a01fdc99c
leemur <jon.lee.murray@gmail.com>
2017-09-03 13:04:34 -0300

    refactoring code

--- main.py
+++ main.py
@@ -1,30 +1,44 @@
 import json
 import requests
 
+# Power all lights to :power: and print status code
 def powerOnAll(power):
     base_url = 'http://192.168.2.22/api/MhMITSZ9EgJ2z45790rlMTZi02sXlznW82AP6Yln/'
     lights_get = requests.get(base_url + 'lights')
     lights = lights_get.json()
     payload = json.dumps({'on': power})
 
-    for light in lights:
-        url = base_url + 'lights/' + light + '/state'
-        light_put = requests.put(url, data=payload)
-        print(light_put.json())
+    powerText = 'off'
+    if power == True:
+        powerText = 'on'
+    print('\n--- Power ' + powerText + ' ---')
 
+    for lightId in lights:
+        light = lights[lightId]
+        url = base_url + 'lights/' + lightId + '/state'
+        light_put = requests.put(url, data=payload)
+
+        print( str(light['name']) + ': ' + str(light_put.status_code) )
+
+# Print light names + current power status
 def printLightInfo():
     base_url = 'http://192.168.2.22/api/MhMITSZ9EgJ2z45790rlMTZi02sXlznW82AP6Yln/'
     lights_request = requests.get(base_url + 'lights')
     lights = lights_request.json()
 
-    for light in lights:
-        name = lights[light]['name']
-        state = lights[light]['state']
-        print( str(name) + ': ' + str(state['on']) )
+    print('\n--- Light Status ---')
+
+    for lightId in lights:
+        light = lights[lightId]
+        powerText = 'off'
+        if light['state']['on'] == True:
+            powerText = 'on'
+
+        print( str(light['name']) + ': ' + powerText )
 
 
 if __name__ == '__main__':
-    print('\n--- Power On ---')
-    powerOnAll(True)
-    print('\n--- Light Information ---')
+    #username: MhMITSZ9EgJ2z45790rlMTZi02sXlznW82AP6Yln
+    powerOnAll(False)
+    #powerOnAll(True)
     printLightInfo()
